0c447ec8639ad0bc5ed1904ed37fa1faf4ed716a
Eddy Davila <EdDavila@fullsail.edu>
2013-08-05 11:37:53 -0500

    Week1 First Commit
    Due tuesday

---
+++ ASDI WEEK1/js/main.js
@@ -0,0 +1,31 @@
+// Eddy Davila
+// ASDI 1308
+// WEEK 1
+// TEMPLATE
+// Full Sail University
+
+//alert("JavaScript is Alive!!!");
+var a = 600;
+if (a < 500) {
+	alert("Testing JS");
+}else{
+	alert("Test JS File ");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
